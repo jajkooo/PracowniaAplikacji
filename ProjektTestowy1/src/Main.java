b21ecb750e36b9e0354d3d69d261941a01c251ed
Bartek Kura <kurtoxz@gmail.com>
2026-09-08 09:45:01 +0200

    cwiczenia java

--- ProjektTestowy1/src/Main.java
+++ ProjektTestowy1/src/Main.java
@@ -1,13 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+void main() {
+    int calkowita = 5;
+    double rzeczywista = 1.5;
+
+    System.out.println("\tWartosc zmiennej \"calkowita\" to " + calkowita);
+    System.out.println("\nWartosc zmiennej rzeczywista to " + rzeczywista);
+
+    char litera = 97;
+    String napis = "napis";
+
+    System.out.println(litera);
+    System.out.println(napis);
+
+    boolean czyPrawda= true; //false
+    System.out.println(czyPrawda);
+
+    System.out.println("wartosc 1 + 2 + 3 = " + (1 + 2 + 3));
+
+    System.out.println(1 + 2 + 3 + " to wartosc 1 + 2 + 3");
+
+    System.out.println("Podaj wiek: ");
+
+    Scanner sc = new Scanner(System.in);
+
+    int wiek = sc.nextInt();
 }
