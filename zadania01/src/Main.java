fdba5273c3944eb5c593cc376c0d5c5a7c1d42ba
Bartosz Kura <policjagorenice@rejdych.ponczki>
2026-09-11 14:41:50 +0200

    zad4

--- zadania01/src/Main.java
+++ zadania01/src/Main.java
@@ -11,4 +11,11 @@ void main() {
 
     /* zad 3*/
     System.out.println("Mam na imię " + imie + ", mam " + (2026 - urodzenie) + " lat i będę pisać maturę za " + ulamek + " roku.");
+
+    /* zad 4*/
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Aktualna temperatura c: ");
+    double stopnie = scanner.nextDouble();
+    double farenhajt = 1.8 * stopnie + 32.0;
+    System.out.println("Aktualna temperatura w farenhajtach " + farenhajt);
 }
