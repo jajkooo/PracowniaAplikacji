121f0cd3560c08f57fb1d1e41c5df786cc0ab2cf
Bartosz Kura <policjagorenice@rejdych.ponczki>
2026-09-11 14:34:20 +0200

    zad3

--- zadania01/src/Main.java
+++ zadania01/src/Main.java
@@ -6,8 +6,9 @@ void main() {
 
     /* zad 2*/
     String imie = "jajkooo";
-    int urodzenie = 2000;
+    int urodzenie = 2007;
     double ulamek = 0.66;
 
     /* zad 3*/
+    System.out.println("Mam na imię " + imie + ", mam " + (2026 - urodzenie) + " lat i będę pisać maturę za " + ulamek + " roku.");
 }
