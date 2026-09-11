1c96e511abe1583de9da7bd4032f2d53b3710082
Bartosz Kura <policjagorenice@rejdych.ponczki>
2026-09-11 14:32:06 +0200

    zad2

--- zadania01/src/Main.java
+++ zadania01/src/Main.java
@@ -3,4 +3,11 @@ void main() {
     System.out.println("Adam");
     System.out.println("Monia");
     System.out.println("Klaudek");
+
+    /* zad 2*/
+    String imie = "jajkooo";
+    int urodzenie = 2000;
+    double ulamek = 0.66;
+
+    /* zad 3*/
 }
